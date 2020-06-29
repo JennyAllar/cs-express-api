f511a769ce1dd4cc7b431dc79c0dd7a3b5a4bd2d
Jenny Allar <jenny.allar@techstars.com>
2020-06-28 22:02:14 -0600

    clean up linting rules

--- src/routes/index.jsx
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const indexRouter = express.Router();
-
-/* GET home page. */
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to CryoSearch API' }));
-
-export default indexRouter;
